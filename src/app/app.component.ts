2469586e9293324a30203834ad3d9c0e819d57be
Mohit Tiwari <themohittiwari@gmail.com>
2022-06-17 20:02:12 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  previewValue = true;
+
+  public listItems = [
+    {
+      id: 0,
+      name: 'Books',
+      purchased: false,
+    },
+
+    {
+      id: 1,
+      name: 'Pens',
+      purchased: false,
+    },
+    {
+      id: 2,
+      name: 'T-shirt',
+      purchased: false,
+    },
+    {
+      id: 3,
+      name: 'Groceries',
+      purchased: false,
+    },
+    {
+      id: 4,
+      name: 'Shoes',
+      purchased: false,
+    },
+    {
+      id: 5,
+      name: 'Vegetables',
+      purchased: false,
+    },
+    {
+      id: 6,
+      name: 'Snacks',
+      purchased: false,
+    },
+  ];
+  public selectedItems = [];
+
+  onToggle() {
+    this.previewValue = !this.previewValue;
+  }
+
+  sendSelectedItems(data: any) {
+    this.selectedItems = data;
+  }
+
+  getLeftItems(obj: any) {
+    this.listItems = obj.remainingList;
+    console.log(obj.remainingList);
+    this.selectedItems = obj.remainingSelected;
+  }
 }
--- src/app/list/list.component.spec.ts
+++ src/app/list/list.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TodoComponent } from './todo.component';
+import { ListComponent } from './list.component';
 
-describe('TodoComponent', () => {
-  let component: TodoComponent;
-  let fixture: ComponentFixture<TodoComponent>;
+describe('ListComponent', () => {
+  let component: ListComponent;
+  let fixture: ComponentFixture<ListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TodoComponent ]
+      declarations: [ ListComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(TodoComponent);
+    fixture = TestBed.createComponent(ListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
